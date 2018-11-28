941505530ae8da438ac16287d7d513635f8d14f0
Lennart Jern <lennart.jern@elastisys.com>
2018-11-28 09:46:07 +0100

    Added scripts from kubernetes.io

---
+++ scripts/install-docker.sh
@@ -0,0 +1,34 @@
+## Install prerequisites.
+yum install yum-utils device-mapper-persistent-data lvm2
+
+## Add docker repository.
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+## Install docker.
+yum update && yum install docker-ce-18.06.1.ce
+
+## Create /etc/docker directory.
+mkdir /etc/docker
+
+# Setup daemon.
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2",
+  "storage-opts": [
+    "overlay2.override_kernel_check=true"
+  ]
+}
+EOF
+
+mkdir -p /etc/systemd/system/docker.service.d
+
+# Restart docker.
+systemctl daemon-reload
+systemctl restart docker
